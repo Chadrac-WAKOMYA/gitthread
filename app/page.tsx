930be8265f0f11288dfec764fda6d4c8be3b0200
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-12-29 15:31:34 +0200

    first usage of input

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return <>
     <Button>Clic Me</Button>
+    <Input/>
   </>
 }
