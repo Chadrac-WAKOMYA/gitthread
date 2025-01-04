180609d3eac0fe4e0f21307d27b4e826f17091c0
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2025-01-04 16:53:01 +0200

    ThemeToggle.tsx contenu added1

--- app/src/theme/ThemeToggle.tsx
+++ app/src/theme/ThemeToggle.tsx
@@ -1,9 +1,16 @@
-import { useTheme, UseThemeProps } from 'next-themes'
+import { Button } from '@/components/ui/button'
+import { useTheme} from 'next-themes'
+import { Moon, SunMedium} from 'lucide-react'
 import React from 'react'
 
 export const ThemeToggle = () => {
     const  {setTheme, theme} = useTheme()
   return (
-    <div>ThemeToggle</div>
+    <Button
+    variant="ghost"
+    size="sm"
+    onClick={()=>{
+        setTheme(theme=="light"?"dark":"light")
+    }}>ThemeToggle</Button>
   )
 }
