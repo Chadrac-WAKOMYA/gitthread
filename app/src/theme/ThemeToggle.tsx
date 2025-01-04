d14a9c92b5dbb0c2107c84aedbff03c8a4661534
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2025-01-04 17:08:20 +0200

    Moon retouch

--- app/src/theme/ThemeToggle.tsx
+++ app/src/theme/ThemeToggle.tsx
@@ -21,7 +21,7 @@ export const ThemeToggle = () => {
         />
         <Moon
             size={20}
-            className="rotate-90 scale-0 scale-100 transition-all dark:-rotate-0 dark:scale-100"
+            className="rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100"
         />
         <span className="sr-only">Toggle Theme</span>
     </Button>
