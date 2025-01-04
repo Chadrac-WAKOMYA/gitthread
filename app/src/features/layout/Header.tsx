4d1fbf60e292c02fce30b1a30b7a6e2ad71e413c
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2025-01-04 17:05:07 +0200

    ThemeToggle in the header

--- app/src/features/layout/Header.tsx
+++ app/src/features/layout/Header.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import { ThemeToggle } from '../../theme/ThemeToggle'
 
 export const Header = async() => {
   return (
     <header className='boder-b border-b-accent'>
         <div className='container flex items-center py-2 max-w-lg m-auto gap-1'>
             <h2 className='texte-2xl font-bold mr-auto '>GitThread</h2>
-            <Button>Demon</Button>
+            <ThemeToggle/>
         </div>
     </header>
   )
