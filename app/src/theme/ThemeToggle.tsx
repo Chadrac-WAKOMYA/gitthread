fe00702e42d21a71a1ed4de0a21b0ee35e2aeb9c
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2025-01-04 17:01:41 +0200

    Moon added

--- app/src/theme/ThemeToggle.tsx
+++ app/src/theme/ThemeToggle.tsx
@@ -7,10 +7,20 @@ export const ThemeToggle = () => {
     const  {setTheme, theme} = useTheme()
   return (
     <Button
-    variant="ghost"
-    size="sm"
-    onClick={()=>{
-        setTheme(theme=="light"?"dark":"light")
-    }}>ThemeToggle</Button>
+        variant="ghost"
+        size="sm"
+        onClick={()=>{
+            setTheme(theme=="light"?"dark":"light")
+        }}
+    >
+        <SunMedium
+            size={20}
+            className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Moon
+            size={20}
+            className="rotate-90 scale-0 scale-100 transition-all dark:-rotate-0 dark:scale-100"
+        />
+    </Button>
   )
 }
